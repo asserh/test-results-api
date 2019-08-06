26191dc195b379200dbb3f03c9f15ffb882ba2ed
Asser Hakala <asserhakala@gmail.com>
2019-08-06 12:01:03 +0200

    chore: edit config

--- config/production.ts
+++ config/production.ts
@@ -1,8 +1,4 @@
 module.exports = {
   port: 80,
   host: '0.0.0.0',
-  aws: {
-    bucket: 'stored-results',
-    endpoint: 's3.eu-north-1.amazonaws.com/test-results-b',
-  }
 };
--- config/test.ts
+++ config/test.ts
@@ -3,6 +3,5 @@ module.exports = {
   host: 'localhost',
   aws: {
     bucket: 'test_runs',
-    endpoint: process.env.AWS_ENDPOINT || 's3.eu-north-1.amazonaws.com/test-results-b',
   }
 };
